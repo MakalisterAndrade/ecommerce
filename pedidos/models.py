f19b880ea8c63583606bf385b2e9e5da26297c73
MakalisterAndrade <makalister.andrade@outlook.com>
2023-09-24 15:56:30 -0400

    add mail-send

--- pedidos/models.py
+++ pedidos/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from loja.models import Produto
 
 
+
 class Pedido(models.Model):
     nome = models.CharField(max_length=50)
     sobrenome= models.CharField(max_length=50)
--- pedidos/views.py
+++ pedidos/views.py
@@ -5,6 +5,11 @@ from django.views.generic import CreateView, TemplateView
 from carrinho.carrinho import Carrinho
 from pedidos.forms import PedidoModelForm
 from .models import ItemPedido, Pedido
+from django.conf import settings
+
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 
 class PedidoCreateView(CreateView):
@@ -15,15 +20,36 @@ class PedidoCreateView(CreateView):
     def form_valid(self, form):
         car = Carrinho(request=self.request)
         pedido = form.save()
+
         for item in car:
             ItemPedido.objects.create(pedido=pedido,
                                       produto=item['produto'],
                                       preco=item['preco'],
                                       quantidade=item['quantidade'])
+
         car.limpar()
         self.request.session['idpedido'] = pedido.id
-        return redirect('resumopedido', idpedido=pedido.id)
 
+        # Send the confirmation email
+        subject = 'Confirmation of Your Order'
+        message = 'Thank you for your order!'
+        from_email = settings.EMAIL_HOST_USER
+        recipient_list = [pedido.email]
+
+        # Render the HTML template for the email
+        html_message = render_to_string('email_template.html', {'pedido': pedido})
+
+        # Send the email
+        send_mail(
+            subject,
+            strip_tags(html_message),
+            from_email,
+            recipient_list,
+            html_message=html_message,
+        )
+
+        return redirect('resumopedido', idpedido=pedido.id)
+    
 
 class ResumoPedidoTemplateView(TemplateView):
     template_name = 'resumopedido.html'
